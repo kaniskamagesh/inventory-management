ab670991aa906e4430e4493cc6cb6fee585e9967
kaniska magesh <kaniskaniska007@gmail.com>
2026-04-07 21:39:02 +0530

    add real frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import UserDashboard from "./pages/UserDashboard";
@@ -9,21 +8,13 @@ import AdminDashboard from "./pages/AdminDashboard";
 function App() {
   return (
     <Router>
-
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
         <Route path="/user-dashboard" element={<UserDashboard />} />
-
         <Route path="/staff-dashboard" element={<StaffDashboard />} />
-
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-
       </Routes>
-
     </Router>
   );
 }

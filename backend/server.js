2c7387745787e68a6a3cce811c95c8e0f67f508d
kaniska magesh <kaniskaniska007@gmail.com>
2026-04-07 12:22:01 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 
-app.listen(process.env.PORT, () =>
-  console.log("Server running on port " + process.env.PORT)
-);
+
+// ✅ THIS SHOULD BE LAST LINE
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
